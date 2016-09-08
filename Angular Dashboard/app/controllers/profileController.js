eb3f7316e63aa842bf654bc7b02e47c30f594839
Rizwan Shafiq <rizwanshafiq@Rizwans-MacBook-Pro.local>
2016-09-08 14:44:26 -0500

    updated

--- Angular Dashboard/app/controllers/profileController.js
+++
@@ -1,7 +0,0 @@
-angular.module('dashboardApp').controller('profileController', function($scope, randomUserService, $http, $q, $timeout) {
-
-    $scope.user = {};
-    randomUserService.getUser().then(function(data) {
-        angular.copy(data, $scope.user);
-    });
-});
